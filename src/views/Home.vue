6b80f5478870795ea89169432e3e82e6c5f5cde6
Muntasir Biojid <muntasir2165@hotmail.com>
2021-02-20 14:48:49 -0700

    4-creating login component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,15 @@
 <template>
-  <div class="home">Hello World</div>
+  <div class="home"><Login /></div>
 </template>
 
 <script>
 // @ is an alias to /src
+import Login from "@/components/Login.vue";
 
 export default {
   name: "Home",
-  components: {},
+  components: {
+    Login,
+  },
 };
 </script>
